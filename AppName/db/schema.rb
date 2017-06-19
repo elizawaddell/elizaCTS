e0645dd9a027ec2319cd2d86e77740824638580f
Skyridge Devcamp <skyridge@devcamp.com>
2017-06-19 17:17:16 +0000

    Get Skills.rb

--- AppName/db/schema.rb
+++ AppName/db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD:AppName/db/schema.rb
 ActiveRecord::Schema.define(version: 20170614164250) do
+=======
+ActiveRecord::Schema.define(version: 20170619162434) do
+>>>>>>> Get Skills.rb:db/schema.rb
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +22,13 @@ ActiveRecord::Schema.define(version: 20170614164250) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
